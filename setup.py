51f9725e78f93db70474cb6996a1598c15b49594
Rémy Le Calloch <remy@lecalloch.net>
2024-05-15 16:18:30 +0200

    Change: improve setup.py for includes management

--- setup.py
+++ setup.py
@@ -6,15 +6,13 @@ packages = []
 root_dir = os.path.dirname(__file__)
 if root_dir:
     os.chdir(root_dir)
-accepted_filetypes = (".html", ".png", ".svg", ".js", ".css")
 
-for dirpath, dirnames, filenames in os.walk("ab_plugin_template"):
+for dirpath, dirnames, filenames in os.walk("ab_online"):
     # Ignore dirnames that start with '.'
-    if ('__init__.py' in filenames
-            or any(x.endswith(accepted_filetypes) for x in filenames)):
-        pkg = dirpath.replace(os.path.sep, '.')
+    if "__init__.py" in filenames or "includes" in dirpath:
+        pkg = dirpath.replace(os.path.sep, ".")
         if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
+            pkg = pkg.replace(os.path.altsep, ".")
         packages.append(pkg)
 
 if 'VERSION' in os.environ:
@@ -30,8 +28,8 @@ setup(
     author="Rémy Le Calloch",
     author_email="remy@lecalloch.net",
     license=open('LICENSE.txt').read(),
-    install_requires=[], # dependency management in conda recipe
+    install_requires=[],  # dependency management in conda recipe
     url="https://github.com/Pan6ora/activity-browser-plugin-template",
     long_description=open('README.md').read(),
     description="An empty plugin to start from",
-    )
+)
